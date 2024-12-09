0a29082181cbc7cd9ff19cb7bdf8e5971a86313d
Christoph Hu <christoph.huschenhoefer@gmx.de>
2024-12-09 13:15:23 +0100

    update with a table by ngx-dynamic-table

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu'
 import { NgxDynamicTablePdfComponent, TableIconsComponent } from '../../../ngx-dynamic-table-pdf/src/public-api';
@@ -7,6 +7,8 @@ import { StickyDirective } from '../../../ngx-dynamic-table-pdf/src/lib/directiv
 import { MatTableModule } from '@angular/material/table'
 import { MatSortModule } from '@angular/material/sort'
 import { MatPaginatorModule } from '@angular/material/paginator'
+import { NgxDynamicTableComponent, Tableoptions } from '@christophhu/ngx-dynamic-table';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +17,7 @@ import { MatPaginatorModule } from '@angular/material/paginator'
     CommonModule,
     MatMenuModule,
     RouterOutlet,
+    NgxDynamicTableComponent,
     NgxDynamicTablePdfComponent,
     StickyDirective,
     MatPaginatorModule,
@@ -25,8 +28,10 @@ import { MatPaginatorModule } from '@angular/material/paginator'
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent { 
-  daten: any = [
+export class AppComponent implements OnInit { 
+  isVisible: boolean = false
+
+  data: any = [
     { id: '1', name: 'Tim', date: '01.01.2024 00:00:59', ort: 'Berlin', checked: false, description: 'Test1' },
     { id: '2', name: 'Tom', date: '01.01.2023 00:00:59', ort: 'Hamburg', checked: false, description: 'Test2' },
     { id: '3', name: 'Thomas', date: '01.02.2023 00:00:59', ort: 'Dresden', checked: false, description: 'Test3' },
@@ -34,4 +39,38 @@ export class AppComponent {
     { id: '5', name: 'Markus', date: '04.02.2023 00:00:59', ort: 'Köln', checked: false, description: 'Hallo2' },
     { id: '6', name: 'Rene', date: '04.02.2023 00:01:59', ort: 'Köln', checked: false, description: 'Hallo2' }
   ]
+
+  data$: Observable<any> = of([
+    { id: '1', name: 'Tim', date: '01.01.2024 00:00:59', ort: 'Berlin', checked: false, description: 'Test1' },
+    { id: '2', name: 'Tom', date: '01.01.2023 00:00:59', ort: 'Hamburg', checked: false, description: 'Test2' },
+    { id: '3', name: 'Thomas', date: '01.02.2023 00:00:59', ort: 'Dresden', checked: false, description: 'Test3' },
+    { id: '4', name: 'Martin', date: '03.02.2023 00:00:59', ort: 'München', checked: false, description: 'Halllo1' },
+    { id: '5', name: 'Markus', date: '04.02.2023 00:00:59', ort: 'Köln', checked: false, description: 'Hallo2' },
+    { id: '6', name: 'Rene', date: '04.02.2023 00:01:59', ort: 'Köln', checked: false, description: 'Hallo2' }
+  ])
+
+  tableoptions: Tableoptions = {
+    columns: [
+      { id: '1', name: 'id', header: 'ID', cell: 'id', hidden: true, sortable: true },
+      { id: '2', name: 'name', header: 'Name', cell: 'name', hidden: false, sortable: true },
+      { id: '3', name: 'date', header: 'Datum/Zeit', cell: 'date', pipe: { name: DatePipe, args: 'dd.MM.YYYY HH:mm:ss'}, hidden: false, sortable: true },
+      { id: '4', name: 'ort', header: 'Ort', cell: 'ort', hidden: false, sortable: true },
+    ],
+    columnFilter: ['name', 'date', 'ort'],
+    columnNames: ['name', 'date', 'ort'],
+    isExpandable: false,
+    checkbox: false,
+    count: false,
+    paginator: false,
+    sortRowManual: false,
+    unread: false,
+    sortColumn: 'date',
+    sortStart: 'asc'
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isVisible = true
+    },2000)
+  }
 }
